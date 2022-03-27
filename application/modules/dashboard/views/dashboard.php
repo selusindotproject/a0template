bb9cd0359e60c25dc25a200cdcdca8dcff06b75a
selusindotproject <selusin.project@gmail.com>
2022-03-27 18:48:09 +0700

    .

--- application/modules/dashboard/views/dashboard.php
+++ application/modules/dashboard/views/dashboard.php
@@ -275,7 +275,7 @@
         <!-- AdminLTE App -->
         <script src="<?= base_url() ?>assets/adminlte/dist/js/adminlte.js"></script>
         <!-- AdminLTE for demo purposes -->
-        <script src="<?= base_url() ?>assets/adminlte/dist/js/demo.js"></script>
+        <!-- <script src="<?= base_url() ?>assets/adminlte/dist/js/demo.js"></script> -->
         <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
         <script src="<?= base_url() ?>assets/adminlte/dist/js/pages/dashboard.js"></script>
     </body>
